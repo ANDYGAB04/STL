19c8acc4c93ac106aecc92f395df978bf18970f4
Andrei <suhan.mhai.r4q@student.ucv.ro>
2024-11-12 19:19:02 +0200

    Rezolvat problema

--- Bonus.cpp
+++ Bonus.cpp
@@ -26,26 +26,26 @@ int main()
 
     inFile >> no_doctors;
     vector<string> docspeciality;
-	vector<string> docnames;
+    vector<string> docnames;
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> name;
         inFile >> speciality;
         docspeciality.push_back(speciality);
         // cout << name << ' ' << speciality << '\n';
-		docnames.push_back(name);
+        docnames.push_back(name);
     }
-    int i = 0,j=0;
-	vector<string> specialitiesbusy;
+    int i = 0, j = 0;
+    vector<string> specialitiesbusy;
     for (auto name = names.begin(); name < names.end(); name++) {
         bool ok = false;
         for (auto v = docspeciality.begin(); v < docspeciality.end(); v++) {
 
 
-            if (*v == specialities[i]&&find(specialitiesbusy.begin(),specialitiesbusy.end(),specialities[i])==specialitiesbusy.end()) {
+            if (*v == specialities[i] && find(specialitiesbusy.begin(), specialitiesbusy.end(), specialities[i]) == specialitiesbusy.end()) {
                 cout << docnames[j] << " " << *name << endl;
                 j++;
-				specialitiesbusy.push_back(*v);
+                specialitiesbusy.push_back(*v);
                 break;
             }
 
